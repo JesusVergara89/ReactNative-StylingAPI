a621bc8e0994419339649ea90f1efc3945cc9d83
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-07 18:29:21 -0600

    inheritance styles

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ export default function App() {
   return (
     <View style={styles.container}>
 
+      <View style={styles.darkMode}>
+        <Text style={styles.darModeText} >Style  <Text style={styles.boldText} >inheritance</Text></Text>
+      </View>
+
       <View style={[styles.box, styles.backGroundCb, styles.boxShadow]}>
         <Text style={styles.title}>Style sheet API</Text>
       </View>
@@ -11,28 +15,44 @@ export default function App() {
       <View style={[styles.box, styles.backGroundCa]}>
         <Text style={styles.title}>Style sheet API</Text>
       </View>
-      
+
     </View>
   )
 }
 
 const styles = StyleSheet.create({
 
+  darkMode: {
+    backgroundColor: "black",
+    padding: 5
+  },
+
+  darModeText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 25
+  },
+
+  boldText: {
+    fontWeight: "bold"
+  },
+
   container: {
     flex: 1,
     backgroundColor: "lightblue",
     padding: 10,
-    marginTop: 50
+    marginTop: 50,
   },
 
   box: {
-     width: 250,
-     height: 100,
-     paddingVertical: 10,
-     marginVertical: 10,
-     borderWidth: 2,
-     borderColor: "pink",
-     borderRadius: 10
+    width: 250,
+    height: 250,
+    paddingVertical: 10,
+    marginVertical: 10,
+    borderWidth: 2,
+    borderColor: "pink",
+    borderRadius: 10,
+    marginHorizontal: "auto"
   },
 
   backGroundCb: {
